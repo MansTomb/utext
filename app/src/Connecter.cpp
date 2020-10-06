a99547299cc47e56fcacbf0a1ea4e81587880ed4
abalabin <abalabin@student.ucode.world>
2020-10-06 11:28:00 +0300

    Toolbar Actions

--- app/src/Connecter.cpp
+++ app/src/Connecter.cpp
@@ -9,9 +9,13 @@ Connecter& Connecter::instance() {
 void Connecter::ConnectToolBarToEditor(TextEditor *editor) {
     auto actions = toolbar->actions();
 
-    for (const auto &item : actions) {
-        QObject::connect(item, &QAction::triggered, editor, &TextEditor::addText);
-    }
+    QObject::connect(actions[1], &QAction::triggered, editor, &TextEditor::copy);
+    QObject::connect(actions[2], &QAction::triggered, editor, &TextEditor::cut);
+    QObject::connect(actions[3], &QAction::triggered, editor, &TextEditor::paste);
+    QObject::connect(actions[5], &QAction::triggered, editor, &TextEditor::addText);
+    QObject::connect(actions[6], &QAction::triggered, editor, &TextEditor::addText);
+    QObject::connect(actions[8], &QAction::triggered, editor, &TextEditor::undo);
+    QObject::connect(actions[9], &QAction::triggered, editor, &TextEditor::redo);
 }
 
 void Connecter::setToolbar(QToolBar *tb) {
